2fad98b272da370b4f3ee9b8c71f9fda8f43eea4
zabyos <128415296+zabyos@users.noreply.github.com>
2026-04-15 22:36:20 -0300

    Refactor product schema and add pagination
    Added pagination support to product schema.

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,50 +1,17 @@
 import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
 
 const productSchema = new mongoose.Schema({
-
-  title: {
-    type: String,
-    required: true
-  },
-
-  description: {
-    type: String,
-    required: true
-  },
-
-  code: {
-    type: String,
-    required: true,
-    unique: true
-  },
-
-  price: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-
-  status: {
-    type: Boolean,
-    default: true
-  },
-
-  stock: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-
-  category: {
-    type: String,
-    required: true
-  },
-
-  thumbnails: {
-    type: [String],
-    default: []
-  }
-
+  title: { type: String, required: true },
+  description: { type: String },
+  code: { type: String, required: true },
+  price: { type: Number, required: true },
+  status: { type: Boolean, default: true },
+  stock: { type: Number, required: true },
+  category: { type: String, required: true },
+  thumbnails: { type: [String], default: [] }
 });
+
+productSchema.plugin(mongoosePaginate);
 
 export const ProductModel = mongoose.model("products", productSchema);
